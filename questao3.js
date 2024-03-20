7a658f026a1ce506e2db2e2a77d0d5f0f2db715a
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-20 10:51:55 -0400

    questao3 finalizada

---
+++ questao3.js
@@ -0,0 +1,9 @@
+/*
+ Questão 3: Como você pode concatenar duas strings em JavaScript?
+ */
+let nome = "Ana"
+let sobrenome = "freitas"
+resultado;
+
+resultado = nome +" "+ sobrenome;
+console.log(resultado);
