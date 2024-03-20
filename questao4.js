6c05d033b3445dc2eb1194ea6f0b0471368f57ba
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-20 10:52:55 -0400

    questao4 finalizada

---
+++ questao4.js
@@ -0,0 +1,10 @@
+/*
+Questão 4: Qual é a diferença entre == e === em JavaScript?
+
+*/
+let num1 = 1;
+let num2 = "1";
+
+console.log(num1 === num2);
+console.log(typeof num1);
+console.log(typeof num2);
