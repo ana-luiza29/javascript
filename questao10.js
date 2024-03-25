96d94fa2920483dcb1e9f4a0add00bab6491fc38
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-25 09:29:08 -0400

    finalizado questao 10

---
+++ questao10.js
@@ -0,0 +1,16 @@
+/*
+Questão 10: Como você pode remover o último elemento de um array em 
+JavaScript? 
+*/
+let array = [1, 2, 3, 4, 5];
+
+// Removendo o último elemento do array
+let ultimoElementoRemovido = array.pop();
+
+console.log(array); // Saída: [1, 2, 3, 4]
+console.log(ultimoElementoRemovido); // Saída: 5
+
+
+
+
+
