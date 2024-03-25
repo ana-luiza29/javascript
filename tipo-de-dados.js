b0d709d38193efc014fabe3cbb074bb6680c7143
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-25 09:42:04 -0400

    exemplos de resto de tipos de dados

---
+++ tipo-de-dados.js
@@ -0,0 +1,25 @@
+//var nome = "Ana";
+//console.log(typeof(nome));
+
+//var caracter = "A";
+//console.log(typeof(caracter));
+//var idade =  25;
+//console.log(typeof(idade));
+
+//var idade =  1.500;
+//console.log(typeof(salario));
+
+//var ligada = false;
+//console.log(typeof(ligada));
+
+//var desligada = true;
+//console.log(typeof(desligada));
+
+var a = 10;
+//console.log(typeof(a));
+
+var b = "10";
+//console.log(typeof(b));
+
+console.log(a == b);
+console.log(a === b);
