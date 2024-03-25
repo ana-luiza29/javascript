773cfc6ff7cc323ed41986fae8d21a30dcda8c39
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-25 09:34:03 -0400

    finalizado questao 5

---
+++ questao5.js
@@ -0,0 +1,9 @@
+ /*
+Questão 5: Como você pode criar uma função em JavaScript?
+*/
+function saudacao(nome){
+    return "Ola," +nome+ "!"
+}
+
+//console.log(saudacao("Maria")); //Aaida: Ola, Maria!
+(saudacao("Maria")); 
