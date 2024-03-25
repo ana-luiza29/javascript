41f381da297e26d120f1a6cd54b3be2e139af34b
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-25 09:34:27 -0400

    finalizado questao 7

---
+++ questao7.js
@@ -0,0 +1,15 @@
+/*
+Questão 7: Como você pode declarar e atribuir um valor a uma variável em uma única linha em JavaScript?
+*/
+
+// Usando let
+let myVariable = 10;
+
+// Usando const
+const PI = 3.14;
+
+// Usando var
+var x = "Hello";
+
+// Você também pode atribuir valores a múltiplas variáveis em uma única linha:
+let a = 5, b = 10, c = "JavaScript";
