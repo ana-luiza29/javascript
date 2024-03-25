ef19451936ee7796889bf98cbd871367c7646758
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-25 09:31:19 -0400

    finalizado questao 19

---
+++ questao19.js
@@ -0,0 +1,8 @@
+/*
+Questão 19: Como você pode definir uma função de retorno de chamada 
+(callback) em JavaScript? 
+*/
+
+function minhaFuncaoDeCallback(parametro1, parametro2) {
+    // Corpo da função de callback
+}
