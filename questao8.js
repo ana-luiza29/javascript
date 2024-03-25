a9b64a4b6faa134d423615a8a2cfff52a7a2df5d
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-25 09:35:31 -0400

    finalizado questao 8

---
+++ questao8.js
@@ -0,0 +1,6 @@
+/*
+Questão 8: Como você pode acessar o último elemento de um array em JavaScript?
+*/
+let array = [1, 2, 3, 4, 5];
+let ultimoElemento = array[array.length - 1];
+console.log(ultimoElemento); // Saída: 5
