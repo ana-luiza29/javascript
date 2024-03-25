8bc7b57fc165ad2d3dafc96ea3ef41acdf05b044
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-25 09:32:34 -0400

    finalizado questao 26

---
+++ questao26.js
@@ -0,0 +1,8 @@
+/*
+
+Questão 26: Como você pode usar o operador ternário em JavaScript? 
+*/
+let idade = 20;
+let mensagem = idade >= 18 ? "Você é maior de idade" : "Você é menor de idade";
+
+console.log(mensagem); // Saída: "Você é maior de idade"
