5507167a447e455c0118713016088b7c0d40ce8f
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-03-25 09:40:51 -0400

    exemplos de objeto

---
+++ objeto.js
@@ -0,0 +1,10 @@
+let pessoa = {
+    nome: "Joao",
+    idade:  30,
+    profissao: "Engenheiro"
+};
+
+console.log(pessoa.nome);  //Saida: Joao 
+console.log(pessoa.idade);  //Saida: 30 
+console.log(pessoa.profissao);  //Saida: Engenheiro
+
